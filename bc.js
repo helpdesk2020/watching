b55849614be0a908958f428fb3411239cabfd7e7
helpdesk2020 <47177644+helpdesk2020@users.noreply.github.com>
2019-01-31 14:34:02 +0200

    Add files via upload

---
+++ bc.js
@@ -0,0 +1,19 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+const token = "NDA5MDkxODQzOTQzMzAxMTIx.Dw7Szw.EouutUcQlv5IX0wnKy--rwLMWUk"
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : Hatem`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By :Hatem  ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Gamer System`,"http://twitch.tv/Hatem")
+client.user.setStatus("dnd")
+});
+
+
+
+client.login(token);
